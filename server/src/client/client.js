ec7bce12e42ea9f59a9a7e35ddfd0278084a9d69
Sarel <Sarelk@gmail.com>
2021-12-05 21:52:21 +0200

    Server side data loading

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,17 +1,20 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
 import Routes from './Routes'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import reducers from './reducers'
 
 const store = createStore(reducers, {}, applyMiddleware(thunk))
 
 ReactDOM.hydrate(
     <Provider store={store}>
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
+        <BrowserRouter>
+            <div>{renderRoutes(Routes)}</div>
+        </BrowserRouter>
     </Provider>,
     document.querySelector('#root'))
